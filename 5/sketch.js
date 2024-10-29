9072174a495669a96e5676b51c2cd1202d328112
zb656 <145730190+zb656@users.noreply.github.com>
2024-10-28 21:19:41 -0400

    done

--- 5/sketch.js
+++ 5/sketch.js
@@ -1,1 +1,38 @@
 // TODO: cp 02, but use video
+let mCamera;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  mCamera = createCapture(VIDEO);
+  mCamera.size(width, height);
+  mCamera.hide();
+}
+
+function draw() {
+  background(0);
+//   image(mCamera, 0, 0);
+
+  mCamera.loadPixels();
+
+  let rectDim = 16;
+  for (let y = 0; y < height; y += rectDim) {
+    for (let x = 0; x < width; x += rectDim) {
+      let pixIdx = y * mCamera.width + x;
+      let p5Idx = 4 * pixIdx;
+
+      let redVal = mCamera.pixels[p5Idx + 0];
+      let greenVal = mCamera.pixels[p5Idx + 1];
+      let blueVal = mCamera.pixels[p5Idx + 2];
+
+      fill(redVal, greenVal, blueVal);
+      noStroke();
+      let diam = map((greenVal+redVal+blueVal)/3, 0, 255, 32, 8);
+      ellipse(x, y, diam);
+    }
+  }
+  mCamera.updatePixels();
+}
+
+// function mousePressed() {
+//     print(mCamera.width, mCamera.height);
+// }
--- 6/sketch.js
+++ 6/sketch.js
@@ -23,5 +23,12 @@ function setup() {
 function draw() {
   image(video, 0, 0);
 
-  print(faces);
+  if (faces.length > 0) {
+    let keypoints = faces[0].keypoints;
+    noStroke();
+    fill(200,0,0);
+    for (let idx = 0; idx < keypoints.length; idx++) {
+      ellipse(keypoints[idx].x, keypoints[idx].y, 2);
+    }
+  }
 }
